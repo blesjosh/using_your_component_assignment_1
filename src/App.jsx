30426a7de3b59aef119fff098b1ffdd4054d2140
Blessing Joshua <blessingjoshuaoffl@gmail.com>
2025-03-12 09:19:40 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
-import { useState } from 'react';
+import { useState } from "react";
+import "./App.css";
+import PostCard from "./components/postcard";
 
 function App() {
-  const books = [
+  // eslint-disable-next-line no-unused-vars
+  const [posts, setPosts] = useState([
     {
       id: 1,
-      image:
-        'https://tse2.mm.bing.net/th?id=OIP.IIIPIPZYMBOJgxFfWGRtBAHaHa&rs=1&pid=ImgDetMain',
-      name: 'The Great Adventure',
-      genre: 'Fiction',
-      author: 'Jane Doe',
+      profileImage:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg",
+      username: "alice",
+      content: "This is my first post!",
+      isLiked: false,
     },
     {
       id: 2,
-      image: 'https://i.thenile.io/r1000/9781496687197.jpg?r=6085750d133cf',
-      name: 'Mysteries of the Universe',
-      genre: 'Science',
-      author: 'John Smith',
+      profileImage:
+        "https://writestylesonline.com/wp-content/uploads/2016/08/Follow-These-Steps-for-a-Flawless-Professional-Profile-Picture.jpg",
+      username: "bob",
+      content: "Loving this social media app.",
+      isLiked: true,
     },
     {
       id: 3,
-      image: 'https://images3.penguinrandomhouse.com/cover/9781933771106',
-      name: 'History Revisited',
-      genre: 'Non-Fiction',
-      author: 'Emily Johnson',
+      profileImage:
+        "https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png",
+      username: "charlie",
+      content: "Just another day...",
+      isLiked: false,
     },
-    {
-      id: 4,
-      image:
-        'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1700317474i/202368563.jpg',
-      name: 'Culinary Delights',
-      genre: 'Cooking',
-      author: 'Michael Brown',
-    },
-  ];
+  ]);
 
-  return <>{/* write code to display all the books using */}</>;
+  return (
+    <div className="app">
+      {posts.map((post) => (
+        <PostCard
+          key={post.id}
+          profilePic={post.profileImage}
+          username={post.username}
+          content={post.content}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
